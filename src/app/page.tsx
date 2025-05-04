844fbbd3b0f69b8f26dc0718e49b49f144e5b770
DevPBDias <devpbdias@gmail.com>
2025-05-04 17:38:41 -0300

    fix: hide floatnavbar for mobile dvc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { navLinks } from "@/constants/navLinks";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <FloatingNav navItems={navLinks} />
+      <FloatingNav className="hidden md:flex" navItems={navLinks} />
       <Hero />
       <Cases />
       <About />
