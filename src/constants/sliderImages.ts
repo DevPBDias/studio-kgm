8d2c29432af8780806558299e236822c03f650d4
DevPBDias <devpbdias@gmail.com>
2025-11-24 11:45:34 -0300

    fix: added new image for trybetunes

--- src/constants/sliderImages.ts
+++ src/constants/sliderImages.ts
@@ -3,7 +3,7 @@ import max from "@/assets/cases/max.png";
 import personal from "@/assets/cases/Personal.png";
 import ftbWebsite from "@/assets/cases/ftbwebsite.png";
 import pomodoro from "@/assets/cases/angular-todo-pomodoro.png";
-import trybetunes from "@/assets/cases/trybetunes.jpg";
+import trybetunes from "@/assets/cases/trybetunes.png";
 import indiApp from "@/assets/cases/Nelore-App.png";
 import Magick from "@/assets/cases/Magick.png";
 
